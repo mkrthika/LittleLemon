b6e47f39a063196c2b963ea26c787a08db3759aa
Krthika <muthukrthika@gmail.com>
2024-07-03 12:46:48 +0400

    Changed the title

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,9 @@ import Booking from './Booking';
 import Main from "./Main";
 
 test('Renders the BookingForm heading', () => {
-    /*render(<Booking />); */
-    /* const headingElement = screen.getByText("Book your Table");
-    expect(headingElement).toBeInTheDocument(); */
+    render(<Booking />);
+    const headingElement = screen.getByText("Book your Table");
+    expect(headingElement).toBeInTheDocument(); 
     render(<Main />);
     const mockFunc =  jest.fn();
     mockFunc();
